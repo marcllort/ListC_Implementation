f40f9d8ae2ba18482768110aafca10772a7be07f
marcllort <mac12llm@gmail.com>
2018-09-29 19:24:37 +0200

    feat- Functions of list.c created
    All the functions are created and without errors, ready to compile. There is not code inside them yet.

---
+++ list.c
@@ -0,0 +1,88 @@
+#include "list.h"
+#include <stdio.h>
+
+
+Llista	LLISTABID_crea () {
+    
+    return 1;
+}
+
+
+int LLISTABID_inserirOrdenat (Llista * l, Element e2){
+    
+    
+    return 1;
+}
+
+
+int	LLISTABID_inserirDarrere (Llista * l, Element e) {    
+    
+
+    return 1;
+}
+
+int LLISTABID_inserirDavant (Llista * l, Element e) {
+    
+
+    return 1;
+}
+
+
+void LLISTABID_consulta (Llista l) {
+
+   
+}
+
+
+int LLISTABID_elimina (Llista * l) {
+    
+
+    return 1;
+}
+
+
+int LLISTABID_avanca (Llista * l) {
+
+    
+    return 1;
+}
+
+
+int LLISTABID_retrocedeix (Llista * l) {
+
+    
+    return 1;
+}
+
+
+int LLISTABID_vesInici (Llista * l) {
+
+    return 1;
+}
+
+
+int LLISTABID_vesFinal (Llista * l) {
+
+    return 1;
+}
+
+
+int LLISTABID_buida (Llista l) {
+  
+    return 1;
+}
+
+
+int LLISTABID_inici (Llista l) {
+    return 1;
+}
+
+
+int LLISTABID_final (Llista l) {
+    return 1;
+}
+
+
+void LLISTABID_destrueix (Llista * l) {
+    
+}
