e2ddfa58b2cc6f057b329cf3071475077e54c471
marcllort <mac12llm@gmail.com>
2018-09-29 19:58:03 +0200

    feat- Main created and ready to test

--- main.c
+++ main.c
@@ -5,7 +5,38 @@
 
 int main (int argc, char** argv) {
 
+	Llista llista;
+	int ok, i;
+	Element e1, e2, e3, e0;
+
+	llista = LLISTABID_crea ();
+  	ok = LLISTABID_vesInici (&llista);
+
 	
+  	for(i = 0 ; i < 10; i++){
+		e0.numero=i+2;
+
+		ok = LLISTABID_inserirDavant (&llista, e0);
+		LLISTABID_consulta (llista);
+	}
+
+
+	//Numeros a afegir ordenats
+	e1.numero=1;
+	e2.numero=6;
+	e3.numero=16;
+
+	ok = LLISTABID_inserirOrdenat (&llista, e1);
+	ok = LLISTABID_inserirOrdenat (&llista, e2);
+	ok = LLISTABID_inserirOrdenat (&llista, e3);
+	
+	
+
+  	ok = LLISTABID_vesFinal (&llista);
+	for(i = 0 ; i < 10+3 ; i++){
+		LLISTABID_consulta (llista);
+		LLISTABID_avanca (&llista);
+	}
 
 return 1;
 }
